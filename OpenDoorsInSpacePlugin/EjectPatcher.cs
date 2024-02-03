839d6c95280291afccac7de2761b42a6bb98cf66
TitaniumTurbine <63096937+TitaniumTurbine@users.noreply.github.com>
2024-02-03 17:09:16 +0000

    start on opendoorsinspace plugin

---
+++ OpenDoorsInSpacePlugin/EjectPatcher.cs
@@ -0,0 +1,95 @@
+﻿using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace OpenDoorsInSpacePlugin
+{
+    internal class EjectPatcher : NetworkBehaviour
+    {
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(StartOfRound), "playersFiredGameOver", MethodType.Enumerator)]
+        static IEnumerable TranspileMoveNext(IEnumerable e)
+        {
+            var s = StartOfRound.Instance;
+
+            s.shipDoorsAnimator.SetBool("OpenInOrbit", true);
+            s.shipDoorAudioSource.PlayOneShot(s.airPressureSFX);
+            s.starSphereObject.SetActive(value: true);
+            s.starSphereObject.transform.position = GameNetworkManager.Instance.localPlayerController.transform.position;
+            yield return new WaitForSeconds(0.25f);
+            s.suckingPlayersOutOfShip = true;
+            s.suckingFurnitureOutOfShip = true;
+            PlaceableShipObject[] array = UnityEngine.Object.FindObjectsOfType<PlaceableShipObject>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].parentObject == null)
+                {
+                    Debug.Log("Error! No parentObject for placeable object: " + s.unlockablesList.unlockables[array[i].unlockableID].unlockableName);
+                }
+
+                array[i].parentObject.StartSuckingOutOfShip();
+                if (s.unlockablesList.unlockables[array[i].unlockableID].spawnPrefab)
+                {
+                    Collider[] componentsInChildren = array[i].parentObject.GetComponentsInChildren<Collider>();
+                    for (int j = 0; j < componentsInChildren.Length; j++)
+                    {
+                        componentsInChildren[j].enabled = false;
+                    }
+                }
+            }
+
+            GameNetworkManager.Instance.localPlayerController.inSpecialInteractAnimation = true;
+            GameNetworkManager.Instance.localPlayerController.DropAllHeldItems();
+            HUDManager.Instance.UIAudio.PlayOneShot(s.suckedIntoSpaceSFX);
+            yield return new WaitForSeconds(6f);
+            SoundManager.Instance.SetDiageticMixerSnapshot(3, 2f);
+            HUDManager.Instance.ShowPlayersFiredScreen(show: true);
+            yield return new WaitForSeconds(2f);
+            s.starSphereObject.SetActive(value: false);
+            s.shipDoorAudioSource.Stop();
+            s.speakerAudioSource.Stop();
+            s.suckingFurnitureOutOfShip = false;
+            if (s.IsServer)
+            {
+                GameNetworkManager.Instance.ResetSavedGameValues();
+            }
+
+            Debug.Log("Calling reset ship!");
+            s.ResetShip();
+            UnityEngine.Object.FindObjectOfType<Terminal>().SetItemSales();
+            yield return new WaitForSeconds(6f);
+            s.shipAnimatorObject.gameObject.GetComponent<Animator>().SetBool("AlarmRinging", false);
+            GameNetworkManager.Instance.localPlayerController.TeleportPlayer(s.playerSpawnPositions[GameNetworkManager.Instance.localPlayerController.playerClientId].position);
+            s.shipDoorsAnimator.SetBool("OpenInOrbit", false);
+            s.currentPlanetPrefab.transform.position = s.planetContainer.transform.position;
+            s.suckingPlayersOutOfShip = false;
+
+            var prop = s.GetType().GetField("choseRandomFlyDirForPlayer", System.Reflection.BindingFlags.NonPublic
+                | System.Reflection.BindingFlags.Instance);
+            prop.SetValue(s, false);
+
+            //s.choseRandomFlyDirForPlayer = false;
+            s.suckingPower = 0f;
+            s.shipRoomLights.SetShipLightsOnLocalClientOnly(setLightsOn: true);
+            yield return new WaitForSeconds(2f);
+            if (s.IsServer)
+            {
+                var prop2 = s.GetType().GetField("playersRevived", System.Reflection.BindingFlags.NonPublic
+                | System.Reflection.BindingFlags.Instance);
+                prop2.SetValue(s, (int)prop2.GetValue(s) + 1);
+
+                //s.playersRevived++;
+                yield return new WaitUntil(() => (int)prop2.GetValue(s) >= GameNetworkManager.Instance.connectedPlayers);
+                prop2.SetValue(s, 0);
+                s.EndPlayersFiredSequenceClientRpc();
+            }
+            else
+            {
+                s.PlayerHasRevivedServerRpc();
+            }
+        }
+    }
+}
---
+++ OpenDoorsInSpacePlugin/OpenDoorsInSpacePlugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System.Collections;
+using System.Linq;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OpenDoorsInSpacePlugin
+{
+    [BepInPlugin("TitaniumTurbine.OpenDoorsInSpace", "OpenDoorsInSpace.Plugin", "1.0.0")]
+    public class OpenDoorsInSpacePlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(OpenDoorsInSpacePlugin));
+        }
+
+        [HarmonyPatch(typeof(HangarShipDoor), "PlayDoorAnimation")]
+        [HarmonyPrefix]
+        static bool SetDoorsClosed (bool closed)
+        {
+            var door = SceneManager.GetActiveScene().GetRootGameObjects().ToList().Find(x => x.name == "Environment").GetComponentInChildren<HangarShipDoor>();
+            if (!door.buttonsEnabled && closed == false)
+            {
+                var s = StartOfRound.Instance;
+                int[] endGameStats = new int[4] { s.gameStats.daysSpent, s.gameStats.scrapValueCollected, s.gameStats.deaths, s.gameStats.allStepsTaken };
+                var h = Harmony.CreateAndPatchAll(typeof(EjectPatcher));
+                s.ManuallyEjectPlayersServerRpc();
+                h.UnpatchSelf();
+            }
+            
+            return true;
+        }
+
+    }
+}
